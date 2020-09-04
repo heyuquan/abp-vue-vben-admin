935c445ae1ffa19e43462626c47cf47ba210b3a8
heyuquan <369220123@qq.com>
2020-09-04 18:59:18 +0800

    1

---
+++ Mk.DemoB/src/Mk.DemoB.Application/SerializerAppService/SerializerAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mk.DemoB.SerializerAppService
+{
+    // 程序集：Volo.Abp.Json，接口：IJsonSerializer  abp对接了Newtonsoft
+    // 程序集：Volo.Abp.Serialization， 接口：IObjectSerializer  二进制序列化  可以使用IObjectSerializer<T>来做定制化序列化
+    public class SerializerAppService : DemoBAppService
+    {
+        public SerializerAppService()
+        {
+            
+        }
+
+
+    }
+}
