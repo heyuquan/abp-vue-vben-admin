88dab28fb895f9bd9f3175f3984abcf109145e6e
heyuquan <369220123@qq.com>
2020-09-23 09:26:19 +0800

    log

--- Mk.DemoB/src/Mk.DemoB.HttpApi.Host/Program.cs
+++ Mk.DemoB/src/Mk.DemoB.HttpApi.Host/Program.cs
@@ -50,7 +50,7 @@ namespace Mk.DemoB
 #if DEBUG
                 //.WriteTo.Console()  // 在容器中，有时候挂载日志文件异常，导致查不出原因，会需要将日志打印到控制台上
 #endif
-                //.WriteTo.Async(c => c.File("Logs/logs.txt"))
+                .WriteTo.Async(c => c.File("Logs/logs.txt"))
                 .WriteTo.Elasticsearch(ConfigureElasticSink(cfg, env))
                 .ReadFrom.Configuration(cfg)
                 .CreateLogger();
