0c84902251576959023771bf78acc3589b76edbc
369220123@qq.com <369220123@qq.com>
2021-12-01 14:30:58 +0800

    解决发布问题

--- aspnetcore/framework/src/Leopard.AspNetCore.Mvc/Microsoft/AspNetCore/Builder/TestMiddleware.cs
+++ aspnetcore/framework/src/Leopard.AspNetCore.Mvc/Microsoft/AspNetCore/Builder/TestMiddleware.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,27 @@ namespace Microsoft.AspNetCore.Builder
     public class TestMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<TestMiddleware> _logger;
 
 
-        public TestMiddleware(RequestDelegate next)
+        public TestMiddleware(RequestDelegate next, ILogger<TestMiddleware> logger)
         {
             this._next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
+            StringBuilder sbuilder = new StringBuilder(128);
+            sbuilder.Append("TestMiddleware==>>");
+
+            sbuilder.Append("Headers==>>");
+            foreach (var item in context.Request.Headers)
+            {
+                sbuilder.AppendFormat("{0}={1};", item.Key, item.Value);
+            }
+
+            _logger.LogInformation(sbuilder.ToString());
             // Do something...
             await _next(context);
         }
