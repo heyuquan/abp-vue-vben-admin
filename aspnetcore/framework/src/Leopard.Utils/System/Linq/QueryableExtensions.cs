121578f3e13e8822a53a965d1d096a332f4abe65
heyuquan <369220123@qq.com>
2022-05-26 22:07:43 +0800

    22

--- aspnetcore/framework/src/Leopard.Utils/System/Linq/QueryableExtensions.cs
+++ aspnetcore/framework/src/Leopard.Utils/System/Linq/QueryableExtensions.cs
@@ -1,7 +1,9 @@
-﻿using Leopard.Utils;
+﻿using JetBrains.Annotations;
+using Leopard.Utils;
 using System.Linq.Expressions;
 
-namespace System.Linq
+// 因为结合Abp框架使用，而这个类又是从Volo.Abp.Core/System/Linq下拷贝过来，重名多，所以命名空间改为 System.Linq.Ex
+namespace System.Linq.Ex
 {
     /// <summary>
     /// IQueryable集合扩展方法
@@ -9,19 +11,90 @@ namespace System.Linq
     public static class QueryableExtensions
     {
         /// <summary>
-        /// 根据第三方条件是否为真来决定是否执行指定条件的查询
+        /// Used for paging. Can be used as an alternative to Skip(...).Take(...) chaining.
         /// </summary>
-        /// <param name="source"> 要查询的源 </param>
-        /// <param name="predicate"> 查询条件 </param>
-        /// <param name="condition"> 第三方条件 </param>
-        /// <typeparam name="T"> 动态类型 </typeparam>
-        /// <returns> 查询的结果 </returns>
-        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, bool condition)
+        public static IQueryable<T> PageBy<T>([NotNull] this IQueryable<T> query, int skipCount, int maxResultCount)
         {
-            Checked.NotNull(source, "source");
-            Checked.NotNull(predicate, "predicate");
+            Checked.NotNull(query, nameof(query));
 
-            return condition ? source.Where(predicate) : source;
+            return query.Skip(skipCount).Take(maxResultCount);
+        }
+
+        /// <summary>
+        /// Used for paging. Can be used as an alternative to Skip(...).Take(...) chaining.
+        /// </summary>
+        public static TQueryable PageBy<T, TQueryable>([NotNull] this TQueryable query, int skipCount, int maxResultCount)
+            where TQueryable : IQueryable<T>
+        {
+            Checked.NotNull(query, nameof(query));
+
+            return (TQueryable)query.Skip(skipCount).Take(maxResultCount);
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static IQueryable<T> WhereIf<T>([NotNull] this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            Checked.NotNull(query, nameof(query));
+
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static TQueryable WhereIf<T, TQueryable>([NotNull] this TQueryable query, bool condition, Expression<Func<T, bool>> predicate)
+            where TQueryable : IQueryable<T>
+        {
+            Checked.NotNull(query, nameof(query));
+
+            return condition
+                ? (TQueryable)query.Where(predicate)
+                : query;
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static IQueryable<T> WhereIf<T>([NotNull] this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
+        {
+            Checked.NotNull(query, nameof(query));
+
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static TQueryable WhereIf<T, TQueryable>([NotNull] this TQueryable query, bool condition, Expression<Func<T, int, bool>> predicate)
+            where TQueryable : IQueryable<T>
+        {
+            Checked.NotNull(query, nameof(query));
+
+            return condition
+                ? (TQueryable)query.Where(predicate)
+                : query;
         }
     }
 }
