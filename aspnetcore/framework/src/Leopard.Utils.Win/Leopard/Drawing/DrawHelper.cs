f24945056d9f9b84c3c77a800d7ea8029af16e9f
heyuquan <369220123@qq.com>
2022-06-17 11:51:39 +0800

    2

--- aspnetcore/framework/src/Leopard.Utils.Win/Leopard/Drawing/DrawHelper.cs
+++ aspnetcore/framework/src/Leopard.Utils.Win/Leopard/Drawing/DrawHelper.cs
@@ -63,40 +63,47 @@ namespace Leopard.Drawing
 
         /// <summary>
         /// 给图样Graphics设置为高清高质量模式
-        /// 经打印验证：在操作 Graphics.Draw** 做绘画之前设置，绘制/打印出来的才更加清楚
         /// </summary>
         /// <param name="g"></param>
-        public static void SetGraphicsHighQuality(Graphics g)
+        /// <param name="isDocumentPrint">true-是打印机打印图像；false-只是保存图像文件</param>
+        public static void SetGraphicsHighQuality(Graphics g, bool isDocumentPrint)
         {
-            // Graphics 关于呈现质量与合成模式
-            // https://www.cnblogs.com/del/archive/2009/12/22/1630120.html
-
-            // （AntiAlias：反锯齿）
-
-            // 绘图质量  
+            // 绘图质量
             g.SmoothingMode = SmoothingMode.AntiAlias;  //使绘图质量最高，即消除锯齿   // ps 使用HighQuality在画矩形边框后打印出来，有锯齿
             // 图像合成质量
             g.CompositingQuality = CompositingQuality.HighQuality;
             // drawstring画图，消除锯齿
             g.TextRenderingHint = TextRenderingHint.AntiAlias;
 
-            // 使用内插模式在缩放期间控制图像质量
-            // https://docs.microsoft.com/zh-cn/windows/win32/gdiplus/-gdiplus-using-interpolation-mode-to-control-image-quality-during-scaling-use?redirectedfrom=MSDN
-            // g.InterpolationMode = InterpolationMode.HighQualityBicubic;   设置为这个，DrawImage会模糊...从上面微软官方文档看，这个是最好的，再验证下看看
-            // DrawImage 是设备相关的函数，换言之就是，DrawImage会把屏幕的参数带上，所以，它绘制图像的DPI基本都是96。
-            // Graphics.DrawImage 打出来的图片变模糊问题 
-            // https://www.nuomiphp.com/eplan/399743.html
-            // https://blog.csdn.net/pengcwl/article/details/7868344
-            g.InterpolationMode = InterpolationMode.NearestNeighbor;
-            // 对于NearestNeighbor，您需要将PixelOffsetMode设置为Half，否则顶部和左侧会占用图像的一半像素。这实际上在上方的蓝色边框上可见。
-            // https://www.nuomiphp.com/eplan/399743.html
-            // https://stackoverflow.com/questions/20776605/missing-half-of-first-pixel-column-after-a-graphics-transform-scale
-            // https://stackoverflow.com/questions/10222774/bitmap-not-aligning-properly-in-panel
-            // 像素的偏移模式
-            g.PixelOffsetMode = PixelOffsetMode.Half;
+            if (isDocumentPrint)
+            {
+                // 使用内插模式在缩放期间控制图像质量
+                // https://docs.microsoft.com/zh-cn/windows/win32/gdiplus/-gdiplus-using-interpolation-mode-to-control-image-quality-during-scaling-use?redirectedfrom=MSDN
+                // g.InterpolationMode = InterpolationMode.HighQualityBicubic;   设置为这个，打印出来的图像会模糊...
+                // 原因：http://cn.voidcc.com/question/p-effcogqr-uh.html
+                // 当进行插值时，GDI +通常将像素的中心偏移半个像素。这在缩放时可能会产生不良影响，出现将缩放后的图像向上和向左移动的现象。使用PixelOffsetMode.Half将像素移回到它们“所属”的位置。
+                // 原因：高质量的双线性和双三次插值模式似乎将边缘像素与图像边界之外的假设透明像素混合在一起，从而在边缘创建半透明边缘。
 
-            // C#下解决DrawImage画出来的Image变⼤了的问题
-            // https://wenku.baidu.com/view/8bbb1928b91aa8114431b90d6c85ec3a87c28bc2.html
+                // 所以：打印设置为 NearestNeighbor + Half
+
+                // DrawImage 是设备相关的函数，换言之就是，DrawImage会把屏幕的参数带上，所以，它绘制图像的DPI基本都是96。
+                // Graphics.DrawImage 打出来的图片变模糊问题 
+                // https://www.nuomiphp.com/eplan/399743.html
+                // https://blog.csdn.net/pengcwl/article/details/7868344
+                g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                // 对于NearestNeighbor，您需要将PixelOffsetMode设置为Half，否则顶部和左侧会占用图像的一半像素。这实际上在上方的蓝色边框上可见。
+                // https://www.nuomiphp.com/eplan/399743.html
+                // https://stackoverflow.com/questions/20776605/missing-half-of-first-pixel-column-after-a-graphics-transform-scale
+                // https://stackoverflow.com/questions/10222774/bitmap-not-aligning-properly-in-panel
+                // 像素的偏移模式
+                g.PixelOffsetMode = PixelOffsetMode.Half;
+            }
+            else
+            {
+                // 设置 NearestNeighbor 在大图转小图的时候，小图会带锯齿
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            }
         }
     }
 }
