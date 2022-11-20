077f4b5a2323f2fa2f8e7301e2fff6d863e67be2
heyuquan <heyq@mw-robot.com>
2022-11-20 13:56:51 +0800

    reflect优化

---
+++ framework-aspnetcore/src/Leopard.Utils/System/Reflect/Dynamic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Reflect
+{
+    /// <summary>
+    /// 动态编译类
+    /// </summary>
+    public class Dynamic
+    {
+        /// <summary>
+        /// 动态赋值
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="value"></param>
+        public static void SetValue(object obj, string fieldName, object value)
+        {
+            FieldInfo info = obj.GetType().GetField(fieldName);
+            info.SetValue(obj, value);
+        }
+        /// <summary>
+        /// 泛型动态赋值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="value"></param>
+        public static void SetValue<T>(object obj, string fieldName, T value)
+        {
+            FieldInfo info = obj.GetType().GetField(fieldName);
+            info.SetValue(obj, value);
+        }
+        /// <summary>
+        /// 动态取值
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static object GetValue(object obj, string fieldName)
+        {
+            FieldInfo info = obj.GetType().GetField(fieldName);
+            return info.GetValue(obj);
+        }
+        /// <summary>
+        /// 动态取值泛型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static T GetValue<T>(object obj, string fieldName)
+        {
+            FieldInfo info = obj.GetType().GetField(fieldName);
+            return (T)info.GetValue(obj);
+        }
+    }
+}
+}
---
+++ framework-aspnetcore/src/Leopard.Utils/System/Reflect/TypeExtensions.cs
@@ -0,0 +1,499 @@
+﻿using JetBrains.Annotations;
+using Leopard.Utils;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    /// <summary>
+    /// 类型<see cref="Type"/>辅助扩展方法类
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 判断当前类型是否可由指定类型派生
+        /// </summary>
+        public static bool IsDeriveClassFrom<TBaseType>(this Type type, bool canAbstract = false)
+        {
+            return type.IsDeriveClassFrom(typeof(TBaseType), canAbstract);
+        }
+
+        /// <summary>
+        /// 判断当前类型是否可由指定类型派生
+        /// </summary>
+        public static bool IsDeriveClassFrom(this Type type, Type baseType, bool canAbstract = false)
+        {
+            Checked.NotNull(type, nameof(type));
+            Checked.NotNull(baseType, nameof(baseType));
+
+            return type.IsClass && !canAbstract && !type.IsAbstract && type.IsBaseOn(baseType);
+        }
+
+        /// <summary>
+        /// 判断类型是否为Nullable类型
+        /// </summary>
+        /// <param name="type"> 要处理的类型 </param>
+        /// <returns> 是返回True，不是返回False </returns>
+        public static bool IsNullableType(this Type type)
+        {
+            return type != null && type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        /// <summary>
+        /// 由类型的Nullable类型返回实际类型
+        /// </summary>
+        /// <param name="type"> 要处理的类型对象 </param>
+        /// <returns> </returns>
+        public static Type GetNonNullableType(this Type type)
+        {
+            if (type.IsNullableType())
+            {
+                return type.GetGenericArguments()[0];
+            }
+            return type;
+        }
+
+        /// <summary>
+        /// 通过类型转换器获取Nullable类型的基础类型
+        /// </summary>
+        /// <param name="type"> 要处理的类型对象 </param>
+        /// <returns> </returns>
+        public static Type GetUnNullableType(this Type type)
+        {
+            if (type.IsNullableType())
+            {
+                NullableConverter nullableConverter = new NullableConverter(type);
+                return nullableConverter.UnderlyingType;
+            }
+            return type;
+        }
+
+        /// <summary>
+        /// 获取类型的Description特性描述信息
+        /// </summary>
+        /// <param name="type">类型对象</param>
+        /// <param name="inherit">是否搜索类型的继承链以查找描述特性</param>
+        /// <returns>返回Description特性描述信息，如不存在则返回类型的全名</returns>
+        public static string GetDescription(this Type type, bool inherit = true)
+        {
+            DescriptionAttribute desc = type.GetAttribute<DescriptionAttribute>(inherit);
+            return desc == null ? type.FullName : desc.Description;
+        }
+
+        /// <summary>
+        /// 获取成员元数据的Description特性描述信息
+        /// </summary>
+        /// <param name="member">成员元数据对象</param>
+        /// <param name="inherit">是否搜索成员的继承链以查找描述特性</param>
+        /// <returns>返回Description特性描述信息，如不存在则返回成员的名称</returns>
+        public static string GetDescription(this MemberInfo member, bool inherit = true)
+        {
+            DescriptionAttribute desc = member.GetAttribute<DescriptionAttribute>(inherit);
+            if (desc != null)
+            {
+                return desc.Description;
+            }
+            DisplayNameAttribute displayName = member.GetAttribute<DisplayNameAttribute>(inherit);
+            if (displayName != null)
+            {
+                return displayName.DisplayName;
+            }
+            DisplayAttribute display = member.GetAttribute<DisplayAttribute>(inherit);
+            if (display != null)
+            {
+                return display.Name;
+            }
+            return member.Name;
+        }
+
+        /// <summary>
+        /// 检查指定指定类型成员中是否存在指定的Attribute特性
+        /// </summary>
+        /// <typeparam name="T">要检查的Attribute特性类型</typeparam>
+        /// <param name="memberInfo">要检查的类型成员</param>
+        /// <param name="inherit">是否从继承中查找</param>
+        /// <returns>是否存在</returns>
+        public static bool HasAttribute<T>(this MemberInfo memberInfo, bool inherit = true) where T : Attribute
+        {
+            return memberInfo.IsDefined(typeof(T), inherit);
+        }
+
+        /// <summary>
+        /// 从类型成员获取指定Attribute特性
+        /// </summary>
+        /// <typeparam name="T">Attribute特性类型</typeparam>
+        /// <param name="memberInfo">类型类型成员</param>
+        /// <param name="inherit">是否从继承中查找</param>
+        /// <returns>存在返回第一个，不存在返回null</returns>
+        public static T GetAttribute<T>(this MemberInfo memberInfo, bool inherit = true) where T : Attribute
+        {
+            var attributes = memberInfo.GetCustomAttributes(typeof(T), inherit);
+            return attributes.FirstOrDefault() as T;
+        }
+
+        /// <summary>
+        /// 从类型成员获取指定Attribute特性
+        /// </summary>
+        /// <typeparam name="T">Attribute特性类型</typeparam>
+        /// <param name="memberInfo">类型类型成员</param>
+        /// <param name="inherit">是否从继承中查找</param>
+        /// <returns>返回所有指定Attribute特性的数组</returns>
+        public static T[] GetAttributes<T>(this MemberInfo memberInfo, bool inherit = true) where T : Attribute
+        {
+            return memberInfo.GetCustomAttributes(typeof(T), inherit).Cast<T>().ToArray();
+        }
+
+        /// <summary>
+        /// 判断类型是否为集合类型
+        /// </summary>
+        /// <param name="type">要处理的类型</param>
+        /// <returns>是返回True，不是返回False</returns>
+        public static bool IsEnumerable(this Type type)
+        {
+            if (type == typeof(string))
+            {
+                return false;
+            }
+            return typeof(IEnumerable).IsAssignableFrom(type);
+        }
+
+        /// <summary>
+        /// 判断当前泛型类型是否可由指定类型的实例填充
+        /// </summary>
+        /// <param name="genericType">泛型类型</param>
+        /// <param name="type">指定类型</param>
+        /// <returns></returns>
+        public static bool IsGenericAssignableFrom(this Type genericType, Type type)
+        {
+            Checked.NotNull(genericType, "genericType");
+            Checked.NotNull(genericType, "type");
+            if (!genericType.IsGenericType)
+            {
+                throw new ArgumentException("该功能只支持泛型类型的调用，非泛型类型可使用 IsAssignableFrom 方法。");
+            }
+
+            List<Type> allOthers = new List<Type> { type };
+            if (genericType.IsInterface)
+            {
+                allOthers.AddRange(type.GetInterfaces());
+            }
+
+            foreach (var other in allOthers)
+            {
+                Type cur = other;
+                while (cur != null)
+                {
+                    if (cur.IsGenericType)
+                    {
+                        cur = cur.GetGenericTypeDefinition();
+                    }
+                    if (cur.IsSubclassOf(genericType) || cur == genericType)
+                    {
+                        return true;
+                    }
+                    cur = cur.BaseType;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 返回当前类型是否是指定基类的派生类
+        /// </summary>
+        /// <param name="type">当前类型</param>
+        /// <param name="baseType">要判断的基类型</param>
+        /// <returns></returns>
+        public static bool IsBaseOn(this Type type, Type baseType)
+        {
+            if (baseType.IsGenericTypeDefinition)
+            {
+                return baseType.IsGenericAssignableFrom(type);
+            }
+            return baseType.IsAssignableFrom(type);
+        }
+
+        /// <summary>
+        /// 返回当前类型是否是指定基类的派生类
+        /// </summary>
+        /// <typeparam name="TBaseType">要判断的基类型</typeparam>
+        /// <param name="type">当前类型</param>
+        /// <returns></returns>
+        public static bool IsBaseOn<TBaseType>(this Type type)
+        {
+            Type baseType = typeof(TBaseType);
+            return type.IsBaseOn(baseType);
+        }
+
+        /// <summary>
+        /// 获取类型的全名，附带所在类库
+        /// </summary>
+        public static string GetFullNameWithModule(this Type type)
+        {
+            return $"{type.FullName},{type.Module.Name.Replace(".dll", "").Replace(".exe", "")}";
+        }
+
+        /// <summary>
+        /// 判断是否是匿名类型
+        /// </summary>
+        /// <param name="obj">对象</param>
+        /// <returns></returns>
+        public static bool IsAnonymous(this object obj)
+        {
+            var type = obj.GetType();
+
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+                   && type.IsGenericType && type.Name.Contains("AnonymousType")
+                   && (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
+                   && type.Attributes.HasFlag(TypeAttributes.NotPublic);
+        }
+
+        /// <summary>
+        /// 获取类型的显示短名称
+        /// </summary>
+        public static string ShortDisplayName(this Type type)
+        {
+            return type.DisplayName(false);
+        }
+
+        /// <summary>
+        /// 获取类型的显示名称
+        /// </summary>
+        public static string DisplayName([NotNull] this Type type, bool fullName = true)
+        {
+            StringBuilder sb = new StringBuilder();
+            ProcessType(sb, type, fullName);
+            return sb.ToString();
+        }
+
+        #region 私有方法
+
+        private static readonly Dictionary<Type, string> _builtInTypeNames = new Dictionary<Type, string>
+        {
+            { typeof(bool), "bool" },
+            { typeof(byte), "byte" },
+            { typeof(char), "char" },
+            { typeof(decimal), "decimal" },
+            { typeof(double), "double" },
+            { typeof(float), "float" },
+            { typeof(int), "int" },
+            { typeof(long), "long" },
+            { typeof(object), "object" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(short), "short" },
+            { typeof(string), "string" },
+            { typeof(uint), "uint" },
+            { typeof(ulong), "ulong" },
+            { typeof(ushort), "ushort" },
+            { typeof(void), "void" }
+        };
+
+        private static void ProcessType(StringBuilder builder, Type type, bool fullName)
+        {
+            if (type.IsGenericType)
+            {
+                var genericArguments = type.GetGenericArguments();
+                ProcessGenericType(builder, type, genericArguments, genericArguments.Length, fullName);
+            }
+            else if (type.IsArray)
+            {
+                ProcessArrayType(builder, type, fullName);
+            }
+            else if (_builtInTypeNames.TryGetValue(type, out var builtInName))
+            {
+                builder.Append(builtInName);
+            }
+            else if (!type.IsGenericParameter)
+            {
+                builder.Append(fullName ? type.FullName : type.Name);
+            }
+        }
+
+        private static void ProcessArrayType(StringBuilder builder, Type type, bool fullName)
+        {
+            var innerType = type;
+            while (innerType.IsArray)
+            {
+                innerType = innerType.GetElementType();
+            }
+
+            ProcessType(builder, innerType, fullName);
+
+            while (type.IsArray)
+            {
+                builder.Append('[');
+                builder.Append(',', type.GetArrayRank() - 1);
+                builder.Append(']');
+                type = type.GetElementType();
+            }
+        }
+
+        private static void ProcessGenericType(StringBuilder builder, Type type, Type[] genericArguments, int length, bool fullName)
+        {
+            var offset = type.IsNested ? type.DeclaringType.GetGenericArguments().Length : 0;
+
+            if (fullName)
+            {
+                if (type.IsNested)
+                {
+                    ProcessGenericType(builder, type.DeclaringType, genericArguments, offset, fullName);
+                    builder.Append('+');
+                }
+                else
+                {
+                    builder.Append(type.Namespace);
+                    builder.Append('.');
+                }
+            }
+
+            var genericPartIndex = type.Name.IndexOf('`');
+            if (genericPartIndex <= 0)
+            {
+                builder.Append(type.Name);
+                return;
+            }
+
+            builder.Append(type.Name, 0, genericPartIndex);
+            builder.Append('<');
+
+            for (var i = offset; i < length; i++)
+            {
+                ProcessType(builder, genericArguments[i], fullName);
+                if (i + 1 == length)
+                {
+                    continue;
+                }
+
+                builder.Append(',');
+                if (!genericArguments[i + 1].IsGenericParameter)
+                {
+                    builder.Append(' ');
+                }
+            }
+
+            builder.Append('>');
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Safely returns the set of loadable types from an assembly.
+        /// </summary>
+        /// <param name="assembly">
+        /// The assembly from which to load types.
+        /// </param>
+        /// <returns>
+        /// The set of types from the assembly, or the subset of types that could be loaded
+        /// if there was any error.
+        /// </returns>
+        public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+        {
+            Checked.NotNull(assembly, nameof(assembly));
+
+            try
+            {
+                return assembly.DefinedTypes.Select(x => x.AsType());
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                return ex.Types.Where(x => x != null);
+            }
+        }
+
+        #region 得到类里面的属性集合
+        private static ConcurrentDictionary<string, object> dictCache = new ConcurrentDictionary<string, object>();
+
+        /// <summary>
+        /// 得到类里面的属性集合
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="columns"></param>
+        /// <returns></returns>
+        public static PropertyInfo[] GetProperties(this Type type, string[] columns = null)
+        {
+            PropertyInfo[] properties = null;
+            if (dictCache.ContainsKey(type.FullName))
+            {
+                properties = dictCache[type.FullName] as PropertyInfo[];
+            }
+            else
+            {
+                properties = type.GetProperties();
+                dictCache.TryAdd(type.FullName, properties);
+            }
+
+            if (columns != null && columns.Length > 0)
+            {
+                //  按columns顺序返回属性
+                var columnPropertyList = new List<PropertyInfo>();
+                foreach (var column in columns)
+                {
+                    var columnProperty = properties.Where(p => p.Name == column).FirstOrDefault();
+                    if (columnProperty != null)
+                    {
+                        columnPropertyList.Add(columnProperty);
+                    }
+                }
+                return columnPropertyList.ToArray();
+            }
+            else
+            {
+                return properties;
+            }
+        }
+
+        /// <summary>
+        /// Gets value of a property by it's full path from given object
+        /// Path eg: Name ; Children.Name ; Volo.Abp.Reflection.ReflectionHelper_Tests+GetValueByPathTestClass.Name ; 
+        /// </summary>
+        public static object GetValueByPath(this object obj, string propertyPath)
+        {
+            return obj.GetValueByPath(obj.GetType(), propertyPath);
+        }
+
+        /// <summary>
+        /// Gets value of a property by it's full path from given object
+        /// Path eg: Name ; Children.Name ; Volo.Abp.Reflection.ReflectionHelper_Tests+GetValueByPathTestClass.Name ; 
+        /// </summary>
+        public static object GetValueByPath(this object obj, Type objectType, string propertyPath)
+        {
+            var value = obj;
+            var currentType = obj.GetType();
+            var objectPath = currentType.FullName;
+            var absolutePropertyPath = propertyPath;
+            if (objectPath != null && absolutePropertyPath.StartsWith(objectPath))
+            {
+                absolutePropertyPath = absolutePropertyPath.Replace(objectPath + ".", "");
+            }
+
+            foreach (var propertyName in absolutePropertyPath.Split('.'))
+            {
+                var property = currentType.GetProperty(propertyName);
+                if (property != null)
+                {
+                    if (value != null)
+                    {
+                        value = property.GetValue(value, null);
+                    }
+                    currentType = property.PropertyType;
+                }
+                else
+                {
+                    value = null;
+                    break;
+                }
+            }
+
+            return value;
+        }
+
+        #endregion
+    }
+}
