58fac93d17b37526bb6220b3aa04b1689732c9f3
heyuquan <heyq@mw-robot.com>
2023-01-01 13:27:28 +0800

    abp updateto602

---
+++ eshop/etc/localhost/build-aspnetcore-abp-update.ps1
@@ -0,0 +1,15 @@
+. "./__build-aspnetcore-common.ps1"
+
+# update volo.abp.cli
+dotnet tool update -g Volo.Abp.Cli
+
+# update all solutions
+$solutionPath = $rootFolder + "/../../aspnetcore/"
+Set-Location $solutionPath
+abp update
+
+$solutionPath = $rootFolder + "/../../../framework/leopard/"
+Set-Location $solutionPath
+abp update
+
+Set-Location $rootFolder
