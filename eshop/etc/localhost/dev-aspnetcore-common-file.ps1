3212a04d7dfba26f331ccf70aea4237224993c87
heyuquan <heyq@mw-robot.com>
2023-02-01 22:58:12 +0800

    配置文件

--- eshop/etc/localhost/dev-aspnetcore-common-file.ps1
+++ eshop/etc/localhost/dev-aspnetcore-common-file.ps1
@@ -10,23 +10,16 @@ foreach ($project in $projectArray) {
     Write-Host "[ $($project.Name) ] begin copy common-file , project-path:$($project.Path)" -ForegroundColor yellow
 
     if (Test-Path $project.Path) {   
-        $cur_sln_common_folder = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$($rootFolder)/aspnetcore/common-file/sln/common/*.*")
-        Write-Host xcopy $cur_sln_common_folder "$($project.Path)\" /s/i/y/d
-        &xcopy $cur_sln_common_folder "$($project.Path)\" /s/i/y/d
-        Write-Host "success copy common-file/sln/common/..." -ForegroundColor green
+        $cur_sln_folder = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$($rootFolder)/aspnetcore/common-file/sln/*.*")
+        Write-Host xcopy $cur_sln_folder "$($project.Path)\" /s/i/y/d
+        &xcopy $cur_sln_folder "$($project.Path)\" /s/i/y/d
+        Write-Host "success copy common-file/sln/..." -ForegroundColor green
 
         if($project.Type -eq "service") {
             $cur_host_folder = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$($rootFolder)/aspnetcore/common-file/host/*.*")
             Write-Host xcopy $cur_host_folder "$($project.RunPath)\" /s/i/y/d
             &xcopy $cur_host_folder "$($project.RunPath)\" /s/i/y/d
             Write-Host "success copy common-file/host..." -ForegroundColor green
-        }
-
-        if(($project.Type -eq "service") -and (($project.Type -eq "gateway")) {
-            $cur_sln_host_folder = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$($rootFolder)/aspnetcore/common-file/sln/host/*.*")
-            Write-Host xcopy $cur_sln_host_folder "$($project.Path)\" /s/i/y/d
-            &xcopy $cur_sln_host_folder "$($project.Path)\" /s/i/y/d
-            Write-Host "success copy common-file/sln/host/..." -ForegroundColor green
         }
     } else {
         Write-Host "can not find path:$($project.Path)" -ForegroundColor red
